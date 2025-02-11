a928aa344a9bb8ee1130c1362f68168a0faacf6f
=CodeJonDave <=jonathan.d.sutton2024@gmail.com>
2025-02-10 21:54:28 -0800

    working on the skeletal structure

---
+++ wsr_backend/src/test/java/com/wsr/wsr_backend/WsrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsr.wsr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
