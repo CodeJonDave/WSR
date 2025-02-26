3d25686fcad3f3ba34cf6fd6f9ce095cdda06898
CodeJonDave <jonathan.d.sutton2024@gmail.com>
2025-02-25 17:29:25 -0800

    revamping and rebuilding

--- database/industries_TABLE.sql
+++
@@ -1,6 +0,0 @@
-CREATE Table industries (
-    industry_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
